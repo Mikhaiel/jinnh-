6361a8ecc47398c9961e027284c89eae6a4b7317
Mikhaiel_Offical <81913716+Mikhaiel@users.noreply.github.com>
2021-08-16 11:25:02 +0530

    JIMBROOTAN ♻️

--- events.js
+++ events.js
@@ -1,5 +1,7 @@
 /* Copyright (C) 2020 Yusuf Usta.
-PINKY V2
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsena - Yusuf Usta
 */
 
 // Komutları burada tutacağız.
