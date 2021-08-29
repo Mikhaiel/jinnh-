d7bf1f2cf39bb20e1a75fa220a8d884b3806c8b3
Mikhaiel_Offical <72667834+realmikhaiel@users.noreply.github.com>
2021-08-29 19:29:10 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -5,6 +5,7 @@ re-editted by Mikhaiel
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
+const fs = require("fs")
 
 const Language = require('../language');
 const Lang = Language.getString('_asena');
