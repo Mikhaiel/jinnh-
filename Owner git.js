2e51fc14000bc4dcafb571e3479813bc692b57ff
Mikhaiel_Offical <72667834+realmikhaiel@users.noreply.github.com>
2021-09-05 16:45:00 +0530

    Create Owner git.js

---
+++ Owner git.js
@@ -0,0 +1,66 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+async function whatsAsena() {
+  const conn = new WAConnection();
+  conn.logger.level = 'warn';
+  conn.version = [2, 2126, 14]
+
+  conn.on('connecting', async () => {
+    console.log(`${chalk.green.bold('Farhan_Dqz ')}${chalk.green.bold('-JulieMwol')}
+${chalk.white.italic('JulieMwol code recipient')}
+${chalk.blue.bold('ℹ️  Connecting JulieMwol... Please wait.')}`);
+  });
+
+  conn.on('open', async () => {
+    console.log(
+      chalk.green.bold('JulieMwol QR Code: '),
+      'JulieMwol:::' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      )
+    );
+    await conn.sendMessage(
+      conn.user.jid,
+      'JulieMwol:::' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      ),
+      MessageType.text
+    );
+    if (conn.user.jid.startsWith('91')) {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ JulieMwol Successfully Scanned✅️*\n*▪️Thanks For Choosing JulieMwol 💞*',
+        MessageType.text
+      );
+    } else {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ JulieMwol Successfully Scanned✅️*\n*▪️Thanks For Choosing JulieMwol 💞*',
+        MessageType.text
+      );
+    }
+    console.log(
+      chalk.green.bold(
+        "EĞER MESAJI KOPYALAMIYORSANIZ LÜTFEN WHATSAPP'I KONTROL EDİN. KENDİ NUMARANIZA QR KODU GÖNDERİLDİ!\n"
+      ),
+      chalk.green.bold(
+        'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+      )
+    );
+    process.exit(0);
+  });
+
+  await conn.connect();
+}
+
+whatsAsena();
